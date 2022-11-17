91d494d01404d77181bec1a1bfbcff1373b34035
takuma39 <takuma.sato.job@gmail.com>
2022-11-18 02:23:31 +0900

    Add Price

---
+++ src/views/Home/components/PriceCardComponent.vue
@@ -0,0 +1,133 @@
+<script setup lang="ts">
+const p_priceCard = defineProps({
+  label: {
+    type: String,
+    required: false,
+  },
+  plan: {
+    type: String,
+    required: false,
+  },
+  price: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  admissionFee: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  rental: {
+    type: String,
+    required: false,
+  },
+  restaurant: {
+    type: String,
+    required: false,
+  },
+  guidance: {
+    type: String,
+    required: false,
+  },
+});
+
+const formatPrice = p_priceCard.price.toLocaleString();
+const formatAdmissionFee = p_priceCard.admissionFee.toLocaleString();
+</script>
+
+<template>
+  <div class="priceCard">
+    <div class="priceCard__label" v-if="p_priceCard.label">{{ p_priceCard.label }}</div>
+    <h3 class="priceCard__plan">{{ p_priceCard.plan }}</h3>
+    <div class="priceCard__priceList">
+      <p class="priceCard__price"><span>¥</span>{{ formatPrice }}</p>
+      <p class="priceCard__admissionFee">（入会金 ¥{{ formatAdmissionFee }}）</p>
+    </div>
+    <div class="priceCard__item">
+      <h4 class="priceCard__itemTitle">レンタル</h4>
+      <p class="priceCard__itemText">
+        {{ p_priceCard.rental }}
+      </p>
+    </div>
+    <div class="priceCard__item">
+      <h4 class="priceCard__itemTitle">レストラン</h4>
+      <p class="priceCard__itemText">{{ p_priceCard.restaurant }}</p>
+    </div>
+    <div class="priceCard__item">
+      <h4 class="priceCard__itemTitle">食事指導</h4>
+      <p class="priceCard__itemText">{{ p_priceCard.guidance }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/sass/app2.scss";
+.priceCard {
+  display: inline-block;
+  position: relative;
+  width: 320px;
+  text-align: center;
+  margin-left: 20px;
+  padding: 40px 20px 0;
+  border-radius: 8px;
+  background-color: #fff;
+}
+
+.priceCard__label {
+  position: absolute;
+  top: -24px;
+  left: 50%;
+  width: 80%;
+  font-size: 20px;
+  font-weight: bold;
+  line-height: 1;
+  color: $color-primary;
+  padding: 10px;
+  transform: translateX(-50%);
+  border: 2px solid $color-primary;
+  border-radius: 50px;
+  background-color: #fff;
+}
+
+.priceCard__plan {
+  @include font-size(26);
+  font-weight: bold;
+  color: $color-primary;
+  margin-bottom: 10px;
+}
+
+.priceCard__priceList {
+  padding-bottom: 20px;
+}
+
+.priceCard__price {
+  @include font-size(48);
+}
+
+.priceCard__price span {
+  @include font-size(40);
+}
+
+.priceCard__item {
+  padding: 30px 0;
+  border-top: 1px solid #ccc;
+}
+
+.priceCard__itemTitle {
+  @include font-size(18);
+  margin-bottom: 5px;
+}
+
+.priceCard__itemText {
+  font-weight: bold;
+}
+
+// $break6: 1140;
+@include mq(o-br6) {
+  .priceCard {
+    width: auto;
+    margin: 0;
+  }
+}
+</style>
--- src/views/Home/components/PriceComponent.vue
+++ src/views/Home/components/PriceComponent.vue
@@ -1,13 +1,124 @@
 <script setup lang="ts">
 import SectionReverse from "@/components/layouts/section/SectionReverseLayout.vue";
+import PriceCard from "./PriceCardComponent.vue";
+import PriceJson from "@/assets/json/price.json";
+import { ref, onMounted } from "vue";
+
+//テス数
+const target = ref<any>();
+
+//変数
+let fade = ref<boolean>(true);
+
+//onMounted
+onMounted(() => {
+  target.value.addEventListener("scroll", () => {
+    if (target.value.scrollLeft > 20) {
+      fade.value = false;
+    } else {
+      fade.value = true;
+    }
+  });
+});
 </script>
 <template>
   <SectionReverse>
     <template v-slot:title-en>Price</template>
     <template v-slot:title-ja>料金</template>
+    <template v-slot:body>
+      <div class="price" ref="target">
+        <PriceCard
+          v-for="n in PriceJson"
+          :key="n.id"
+          :label="n.label"
+          :plan="n.plan"
+          :price="n.price"
+          :admissionFee="n.admissionFee"
+          :rental="n.rental"
+          :restaurant="n.restaurant"
+          :guidance="n.guidance"
+        />
+        <transition name="fade">
+          <div class="price__scroll" v-show="fade">
+            <span class="price__scroll-icon material-icons-round">east</span
+            >スクロール
+          </div>
+        </transition>
+      </div>
+    </template>
   </SectionReverse>
 </template>
 <style scoped lang="scss">
 @import "@/assets/sass/app2.scss";
+.price {
+  margin: 0 -30px;
+  position: relative;
+  white-space: nowrap;
+  margin-top: -25px;
+  padding-top: 25px;
+  padding-right: 20px;
+  overflow-x: auto;
+}
 
+.price__scroll {
+  position: absolute;
+  top: 50%;
+  right: 5%;
+  @include font-size(12);
+  color: #fff;
+  padding: 10px;
+  transform: translateY(-50%);
+  border-radius: 4px;
+  background-color: rgba(0, 0, 0, 0.6);
+}
+
+.price__scroll-icon {
+  display: block;
+  @include font-size(40);
+  text-align: center;
+}
+
+// $break6: 1140;
+@include mq(o-br6) {
+  .price__body {
+    margin: 0;
+  }
+
+  .price {
+    margin: 0;
+    display: grid;
+    padding-right: 0;
+    column-gap: 15px;
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  .price__scroll {
+    display: none;
+  }
+}
+
+.fade {
+  &-enter {
+    &-from {
+      opacity: 0;
+    }
+    &-to {
+      opacity: 1;
+    }
+    &-active {
+      transition: opacity 0.3s;
+    }
+  }
+  &-leave {
+    &-from {
+      opacity: 1;
+    }
+    &-to {
+      opacity: 0;
+    }
+    &-active {
+      transition: opacity 0.3s;
+    }
+  }
+}
 </style>
